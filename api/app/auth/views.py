e43d75c002a01f844f8eabd7a185112828efdae2
Julio Felix <juliocesarfelixhz95@gmail.com>
2019-10-17 02:04:55 -0700

    fixing BE

--- api/app/auth/views.py
+++ api/app/auth/views.py
@@ -1,16 +1,14 @@
 
 from flask import (
     Blueprint, session, g, 
-    request, render_template, redirect, url_for, jsonify,make_response )
+    request, render_template, redirect, url_for, jsonify,make_response, current_app )
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from app.models import Users
 from . import auth
 import datetime
 import jwt 
-# Por que este si se puede importar?
-from run import app
-print("what is app right now? : ", app)
+
 @auth.route('/logout')
 def logout():
     session.pop("idUser", None)
@@ -33,7 +31,7 @@ def login():
         return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
 
     if check_password_hash(user.password__user, auth.password):
-        token = jwt.encode({'id' : user.id__user, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+        token = jwt.encode({'id' : user.id__user, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, current_app.config['SECRET_KEY'])
 
         return jsonify({'token' : token.decode('UTF-8'), "username": user.username__user } )
 
