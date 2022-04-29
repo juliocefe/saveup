13bb8349137d7cb2180dcda18c9c1f7a1a105afc
juliocefe <juliocesarfelixhz95@gmail.com>
2022-04-28 19:55:01 -0700

    unused import

--- my-app/src/components/ProductsList/index.js
+++ my-app/src/components/ProductsList/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useEffect, Fragment } from "react";
+import React, { useState, useMemo, useEffect } from "react";
 import { Product } from "../Product";
 
 import { List, Item } from "./styles";
